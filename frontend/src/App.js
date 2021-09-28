6ce49e593e55507d498f9c5dd4e26f1e1509c3cb
Joseph Camyre <jwcamry03@gmail.com>
2021-09-28 15:29:27 -0700

    Link frontend to backend api request

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('api/').then((res) => {
+      if (res) {
+        setData(res['data']);
+        console.log(res['data']);
+      }
+    })
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <h1>
+          {!data ? "loading" : data['message']}
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
