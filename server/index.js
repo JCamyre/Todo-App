431908b58cebda21adfb715dfd6b7726414195af
Joseph Camyre <jwcamry03@gmail.com>
2021-09-28 15:18:46 -0700

    Add api endpoint

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const { appendFile } = require("fs");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// api end points
+api.get("/api", (req, res) => {
+    res.json({ message: "yo" });
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
+
