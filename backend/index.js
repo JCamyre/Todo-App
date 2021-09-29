cf801e1a1058c2d3dd3a8190c3ac323e2612ee54
Joseph Camyre <jwcamry03@gmail.com>
2021-09-29 00:04:45 -0700

    Fix accessing api endpoint

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// api end points
+// api end points. app.get() is GET, app.post() is POST
 app.get("/api", (req, res) => {
     res.json({ message: "yo" });
 });
 
-app.get("/api/submit-form", (req, res) => {
+app.post("/api/submit-form", (req, res) => {
     // could use events instead of callbackss
     new formidable.IncomingForm().parse(req, (err, fields, files)    
         .on('fileBegin', (name, file) => {
@@ -20,23 +20,22 @@ app.get("/api/submit-form", (req, res) => {
             file.path = __dirname + '/uploads' + file.name
         })
         .on('field', (name, field) => {
-        console.log('Field', name, field)
+            console.log('Field', name, field)
         })
         .on('file', (name, file) => {
-        console.log('Uploaded file', name, file)
+            console.log('Uploaded file', name, file)
         })
         .on('aborted', () => {
-        console.error('Request aborted by the user')
+            console.error('Request aborted by the user')
         })
         .on('error', (err) => {
-        console.error('Error', err)
-        throw err
+            console.error('Error', err)
+            throw err
         })
         .on('end', () => {
-        res.end()
-        })
+            res.end()
+        }));
     });
-});
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
